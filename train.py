9b58b1c6bc795d2dfa1833558888cdae823ac483
Sanho LEE <stlsh0516@gmail.com>
2023-03-15 07:36:37 +0900

    ready for train.py

---
+++ train.py
@@ -0,0 +1,33 @@
+## necessary modules
+
+
+## Training Parameters
+
+
+
+
+
+## make some function for Converting data type(ndarray <-> tensor)
+# dataset is ndarray type by default.
+# (todo) need to make ToTensor class in dataset file as utility function.
+
+
+
+
+## make network instance
+
+
+
+
+## Preparing Dataset and Dataloader
+
+
+
+## make optimizer and loss function
+
+
+## some works for saving data : checkpoint for network, optimizer and epoch info.
+
+
+## need to consider how to make output string of masked pixels.
+
